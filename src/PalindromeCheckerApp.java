eea29f036139fc6c5e50ca2ac3c296050ac402ba
asthasingh011 <asthaasingh011@gmail.com>
2026-02-20 12:38:47 +0530

    Created Main Class

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        UC1_WelcomeMessage.display();
+        UC2_HardcodedPalindrome.check();
+        UC3_StringReversePalindrome.check();
+        UC4_CharArrayPalindrome.check();
+        UC5_StackPalindrome.check();
+        UC6_QueueStackPalindrome.check();
+        UC7_DequePalindrome.check();
+
+        System.out.println("Program Ended.");
+    }
+}
